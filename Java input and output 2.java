3b1e93b96324d06c7522920f53f26731b85c688d
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-10-05 23:37:46 +0530

    Update Java input and output 2.java

--- Java input and output 2.java
+++ Java input and output 2.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+  {
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
         double d = scan.nextDouble();
